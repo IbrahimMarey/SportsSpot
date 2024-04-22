c53b15385916ae43a52a9dbbb61c74a3ea147b9a
IbrahimMarey <61487676+IbrahimMarey@users.noreply.github.com>
2024-04-22 19:51:51 +0200

    Initial Commit

---
+++ SportsSpot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportsSpot
+//
+//  Created by Khokha on 22/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

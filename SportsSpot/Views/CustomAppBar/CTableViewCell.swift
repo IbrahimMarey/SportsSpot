4836071640ad6cc204bf1e6916a5fe1eed51f6da
Aya Hany <89316483+Ayahany242@users.noreply.github.com>
2024-04-24 15:59:06 +0200

    test
    coreDataTest

---
+++ SportsSpot/Views/CustomAppBar/CTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CTableViewCell.swift
+//  SportsSpot
+//
+//  Created by AYA on 24/04/2024.
+//
+
+import UIKit
+
+class CTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

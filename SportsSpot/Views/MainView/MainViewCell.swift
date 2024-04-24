b74883484e673e06ebbcbb45d0ae563391103729
IbrahimMarey <61487676+IbrahimMarey@users.noreply.github.com>
2024-04-24 11:38:36 +0200

    commit

---
+++ SportsSpot/Views/MainView/MainViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewCell.swift
+//  SportsSpot
+//
+//  Created by Khokha on 23/04/2024.
+//
+
+import UIKit
+
+class MainViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageCell: UIImageView!
+    @IBOutlet weak var cellTitle: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+
+}
